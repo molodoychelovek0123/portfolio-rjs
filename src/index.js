f8695d5cdf65771fa4ce05afc79b8d48ec46242b
boxdeveloper <boxdeveoper@studiobox.dev>
2022-06-23 17:11:17 +0300

    почему не работает? ;c

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {    BrowserRouter,    Routes,    Route } from "react-router-dom";
+// import {    BrowserRouter,    Routes,    Route } from "react-router-dom";
 import './styles/index.scss';
 import App from './App';
 // import AppEdit from "./AppEdit";
@@ -11,13 +11,14 @@ import App from './App';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(document.getElementById('root'));
 root.render( // Тут когда-то была идея использовать router dom, но не получилось не фортануло не повезло, когда нибудь этот участок кода будет переделан, но не сегодня.
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={ <App /> } />
-            {/*<Route path='/edit' element={ <AppEdit /> } />*/}
-        </Routes>
-    </BrowserRouter>
-    // <App/>
+    // <BrowserRouter>
+    //     <Routes>
+    //         <Route path='/' element={ <App /> } />
+    //         {/*<Route path='/edit' element={ <AppEdit /> } />*/}
+    //     </Routes>
+    // </BrowserRouter>
+    <App/>
 );
 
